e0e31fb3ce26b4aaae9ff9b275a65ddc04ebd3a8
KasAndrey <drew.kasatkin28@gmail.com>
2022-09-30 12:04:58 +0300

    Дополнительное ограничение на работу программы

--- labs_2022-fall/src/main/kotlin/Main.kt
+++ labs_2022-fall/src/main/kotlin/Main.kt
@@ -6,18 +6,22 @@ fun main() {
     val input = readln()
     val request = WikipediaRequest(input)
 
-    println("Search \"$input\" results are:")
-    request.results.forEachIndexed { index, result ->
-        println("($index) $result")
-    }
+    if (request.results.isEmpty()) {
+        println("\nNo Results Found")
+    } else {
+        println("Search \"$input\" results are:")
+        request.results.forEachIndexed { index, result ->
+            println("($index) $result")
+        }
 
-    println("\nInput index to open:")
-    try {
-        val inputtedIndex = readln().toInt()
-        request.openWikipediaPage(inputtedIndex)
-    } catch (_: NumberFormatException) {
-        println("ERROR: Entered string is not a valid number")
-    } catch (_: IndexOutOfBoundsException) {
-        println("ERROR: Inputted index is out of range")
+        println("\nInput index to open:")
+        try {
+            val inputtedIndex = readln().toInt()
+            request.openWikipediaPage(inputtedIndex)
+        } catch (_: NumberFormatException) {
+            println("ERROR: Entered string is not a valid number")
+        } catch (_: IndexOutOfBoundsException) {
+            println("ERROR: Inputted index is out of range")
+        }
     }
 }
